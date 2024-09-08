2e5d97e4964453b29d411d1bd1966ca73010cc8a
mohaabbass <mohaa.abbas@gmail.com>
2024-09-08 14:55:04 +0200

    edit mongodb file 3

--- src/lib/mongodb.tsx
+++ src/lib/mongodb.tsx
@@ -1,26 +1,38 @@
 import { MongoClient } from "mongodb";
 
+// MongoDB connection URI from environment or fallback to localhost MongoDB
 const uri: string = process.env.MONGODB_URI || "mongodb://localhost:27017";
+
+// MongoDB connection options (leave it empty if not using additional options)
 const options = {};
 
 let client: MongoClient;
 let clientPromise: Promise<MongoClient>;
 
+// Extend the NodeJS.Global interface to include _mongoClientPromise for TypeScript typing
+declare global {
+  // Extending the NodeJS Global interface to include _mongoClientPromise
+  var _mongoClientPromise: Promise<MongoClient>;
+}
+
+// Throw an error if MongoDB URI is not provided in environment variables
 if (!process.env.MONGODB_URI) {
   throw new Error("Please add your Mongo URI to .env.local");
 }
 
+// Check if we are in development mode
 if (process.env.NODE_ENV === "development") {
-  // In development mode, we use a global variable to maintain the MongoDB connection across hot reloads.
-  if (!(global as any)._mongoClientPromise) {
+  // In development mode, use a global variable to maintain a single connection across hot reloads
+  if (!global._mongoClientPromise) {
     client = new MongoClient(uri, options);
-    (global as any)._mongoClientPromise = client.connect();
+    global._mongoClientPromise = client.connect();
   }
-  clientPromise = (global as any)._mongoClientPromise;
+  clientPromise = global._mongoClientPromise;
 } else {
-  // In production, create a new client for each request.
+  // In production, always create a new connection for each request
   client = new MongoClient(uri, options);
   clientPromise = client.connect();
 }
 
+// Export the clientPromise to be used in the app
 export default clientPromise;
